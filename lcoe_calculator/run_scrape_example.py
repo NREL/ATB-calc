a9f452f90298d574f044570aace1113067c0e04c
Brian Mirletz <brian.mirletz@nrel.gov>
2023-07-13 10:00:46 -0600

    add new battery size ratio term to pv+battery lcoe equation. capex is scraped correctly without changes

--- lcoe_calculator/run_scrape_example.py
+++ lcoe_calculator/run_scrape_example.py
@@ -5,7 +5,7 @@ from datetime import datetime as dt
 from full_scrape import FullScrape
 
 # Data master version on sharepoint - empty string if you haven't renamed the file
-version_string = "_v5.3"
+version_string = "_v7.1"
 
 # Path to data master spreadsheet
 data_master_filename = '../2023-ATB-Data_Master' + version_string + '.xlsx'
