86393b8d357b1748c0465c1ddf0ee894bddaf827
Brian Mirletz <brian.mirletz@nrel.gov>
2025-03-26 18:03:39 -0600

    Record round trip efficiency for psh

--- lcoe_calculator/tech_processors.py
+++ lcoe_calculator/tech_processors.py
@@ -343,6 +343,7 @@ class PumpedStorageHydroProc(TechProcessor):
         ("df_vom", "Variable O&M"),
         ("df_cfc", "CFC"),
         ("df_capex", "CAPEX"),
+        ("df_rte", "Round-Trip Efficiency"),
     ]
 
     metrics = [
@@ -351,6 +352,7 @@ class PumpedStorageHydroProc(TechProcessor):
         ("Fixed Operation and Maintenance Expenses ($/kW-yr)", "df_fom"),
         ("Variable Operation and Maintenance Expenses ($/MWh)", "df_vom"),
         ("Construction Finance Factor", "df_cff"),
+        ("Round-Trip Efficiency", "df_rte"),
     ]
 
 
